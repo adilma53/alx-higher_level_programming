004b4865f1c0370b8f375c9572f6dc5aafe31ee1
adil majhoul <itsadilhadid@gmail.com>
2023-06-07 00:59:16 +0100

    ALX HIGH LEVEL

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(s):
     for c in s:
         if ord(c) >= 97 and ord(c) <= 122:
             up_c = chr(ord(c) - 32)
-            print({}.format(up_c), end="")
+            print("{}".format(up_c), end="")
         else:
-            print({}.format(c), end="")
+            print("{}".format(c), end="")
     print()
