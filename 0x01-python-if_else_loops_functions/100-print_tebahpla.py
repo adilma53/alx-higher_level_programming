1a82c4d0c2aa72a951306f1970b2347152e05810
adil majhoul <itsadilhadid@gmail.com>
2023-06-07 02:49:47 +0100

    ALX HIGH LEVEL

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a') - 1, -1):
-    print(chr(i).upper() if i % 2 == 0 else chr(i).lower(), end='')
-
-print()
+for i in range(122, 96, -1):
+    print(chr(i).lower() if i % 2 == 0 else chr(i).upper(), end='')
