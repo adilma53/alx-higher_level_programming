3c6369752627719f0f7ee122bbb514d71d3f7d54
adil majhoul <itsadilhadid@gmail.com>
2023-06-07 02:34:06 +0100

    ALX HIGH LEVEL

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,9 +4,9 @@ def remove_char_at(str, n):
     if n < 0 or n >= len(str):
         return str
 
-    rturn = ""
+    s = ""
     for i in range(len(str)):
         if i != n:
-            rturn += str[i]
+            s += str[i]
 
-    return rturn
+    return s
