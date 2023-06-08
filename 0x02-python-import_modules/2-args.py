d0bfca8584b1c91e621d182dcd77d5b2297ea0ce
adil majhoul <itsadilhadid@gmail.com>
2023-06-08 23:11:13 +0100

    ALX HIGH LEVEL

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    """test"""
     from sys import argv
-    """ test2 """
-
 
 argv.pop(0)
-
 i = 1
-
 len = len(argv)
 
-print("{} argument:".format(len) if len > 0 else "{} argument.".format(len))
+if len > 0 and len != 1:
+    print("{} arguments:".format(len))
+elif len == 0:
+    print("{} arguments.".format(len))
+elif len == 1:
+    print("{} argument:".format(len))
+
 
 for arg in argv:
     print("{}: {}".format(i, arg))
