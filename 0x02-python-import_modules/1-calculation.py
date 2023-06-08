fde545925a07acc52d8c131993ddb980e5f5a92f
adil majhoul <itsadilhadid@gmail.com>
2023-06-08 21:34:38 +0100

    ALX HIGH LEVEL

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """mini calculator that performs (+,-,*,/)"""
+
+
+a = 10
+b = 5
+
+
+ops = [
+    ('+', add),
+    ('-', sub),
+    ('*', mul),
+    ('/', div)
+]
+
+
+for op, func in ops:
+    result = func(a, b)
+    print('{} {} {} = {}'.format(a, op, b, result))
