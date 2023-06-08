bc2dcd53e3758ef5fc0e006a777d4d07aad56eb7
adil majhoul <itsadilhadid@gmail.com>
2023-06-08 23:17:22 +0100

    ALX HIGH LEVEL

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,12 +4,10 @@ if __name__ == '__main__':
     from sys import argv
 
 argv.pop(0)
-len = len(argv)
 sum = 0
 
 
 for arg in argv:
-    print("{}: {}".format(i, arg))
     sum += int(arg)
 
 print(sum)
