e162330e03b3988b2de5f29d9ee60b6070e89152
adil majhoul <itsadilhadid@gmail.com>
2023-06-09 00:04:18 +0100

    ALX HIGH LEVEL

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@
 if __name__ == '__main__':
     import hidden_4
 
-    names = dir(hidden_4)
+names = dir(hidden_4)
 
-    for name in names:
-        if names[:2] != '__':
-            print('{}'.format(name))
+for name in names:
+    if name[:2] != '__':
+        print('{}'.format(name))
