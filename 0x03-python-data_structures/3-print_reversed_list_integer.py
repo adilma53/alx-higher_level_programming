6ad6f54aefc5b767fd6d5056386e93a5e8ab76da
adil majhoul <itsadilhadid@gmail.com>
2023-06-11 20:08:15 +0100

    ALX HIGH LEVEL

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    """test """
     i = len(my_list) - 1
-    if isinstance(my_list, list):
-        while i >= 0:
-            print("{:d}".format(my_list[i]))
-            i -= 1
+    
+    while i >= 0:
+        print("{:d}".format(my_list[i]))
+        i -= 1
