ce972d31a2a8d05f35f13886eb4b8693e08457b9
adil majhoul <itsadilhadid@gmail.com>
2023-06-12 17:21:35 +0100

    ALX HIGH LEVEL

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+
+    new_list = []
+
+    for i in my_list:
+        if elements % 2 == 0:
+            new_list += true
+        else:
+            new_list += false
+
+    return new_list
