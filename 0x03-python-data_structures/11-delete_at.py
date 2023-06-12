ff5f0b4e9c12935610902a2e9185c5f65e64115f
adil majhoul <itsadilhadid@gmail.com>
2023-06-12 17:16:51 +0100

    ALX HIGH LEVEL

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= length:
         return my_list
 
-    my_list = [x for i, x in enumerate(my_list) if i != idx]
+    my_list.remove(idx)
 
     return my_list
