c9ad2454843f4e3b57fe9e9a0d0e49d3046113eb
adil majhoul <itsadilhadid@gmail.com>
2023-06-14 22:23:08 +0100

    ALX HIGH LEVEL

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+
+    sorted_dict = dict(sorted(a_dictionary.items()))
+
+    for key in sorted_dict:
+        print(key + ':', sorted_dict[key])
