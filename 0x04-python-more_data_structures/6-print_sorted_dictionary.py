c1892b44e905e3bf8bf581d840953c029bd2d84b
adil majhoul <itsadilhadid@gmail.com>
2023-06-14 22:26:34 +0100

    ALX HIGH LEVEL

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,7 @@
 
 def print_sorted_dictionary(a_dictionary):
 
-    for key in sorted(a_dictionary.items()):
-        print(key + ':', a_dictionary[key])
+    sorted_dict = dict(sorted(a_dictionary.items()))
+
+    for key in sorted_dict:
+        print(key + ':', sorted_dict[key])
