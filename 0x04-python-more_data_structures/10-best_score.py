a3fe614b54afcacde4c2740fe595ecbe5fdcc37f
adil majhoul <itsadilhadid@gmail.com>
2023-06-15 00:35:48 +0100

    ALX HIGH LEVEL

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+
+    if a_dictionary is None:
+        return None
+
+    for key in a_dictionary:
+        max_n = a_dictionary[key]
+        break
+
+    
+    key = ""
+    for i,j in a_dictionary:
+        if j > max_n:
+            max_n = j
+            key = i
+    
+    return key
+
+
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
