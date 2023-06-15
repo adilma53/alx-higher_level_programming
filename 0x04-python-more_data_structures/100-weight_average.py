b17ada00ff51420992944634fef349ce51fb5d04
adil majhoul <itsadilhadid@gmail.com>
2023-06-15 02:27:45 +0100

    ALX HIGH LEVEL

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,12 +3,12 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    
+
     s = 0
     t = 0
-    
+
     for i, j in my_list:
         s += i * j
         t += j
-    
+
     return s / t
