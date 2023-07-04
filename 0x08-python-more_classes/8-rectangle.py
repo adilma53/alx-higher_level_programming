be0894515166a0971826497c1ca641ddc873a385
adil majhoul <itsadilhadid@gmail.com>
2023-07-04 02:58:10 +0100

    ALX HIGH LEVEL

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -6,13 +6,13 @@ class Rectangle:
     """Representation of a rectangle"""
 
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        """Initializes the rectangle"""
+        """Initializes the rectangle with width and height"""
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -44,26 +44,39 @@ class Rectangle:
 
     def area(self):
         """Calculates the area of the rectangle"""
-        return self.__width * self.__height
+        return self.__height * self.width
 
     def perimeter(self):
         """Calculates the perimeter of the rectangle"""
-        return 2 * (self.__width + self.__height)
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (2 * (self.__height + self.__width))
 
     def __str__(self):
         """Returns a string representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        rectangle_str = ""
-        for _ in range(self.__height):
-            rectangle_str += str(self.print_symbol) * self.__width + "\n"
-        return rectangle_str[:-1]
+        return ("\n".join(["".join([str(self.print_symbol)
+                for _ in range(self.__width)]) for _ in range(self.__height)]))
 
     def __repr__(self):
         """Returns a string representation of the rectangle object"""
-        return f"Rectangle({self.__width}, {self.__height})"
+        empty_string = "Rectangle(" + str(self.__width)
+        empty_string += ", " + str(self.__height) + ")"
+        return empty_string
 
     def __del__(self):
         """Deletes the rectangle object and prints a message"""
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
+        print('Bye rectangle...')
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on area"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        elif type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
