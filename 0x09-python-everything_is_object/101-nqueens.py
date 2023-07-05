8c02d6141af2c41fd6d8e03292ef4507e138573d
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 20:24:27 +0100

    ALX HIGH LEVEL

---
+++ 0x09-python-everything_is_object/101-nqueens.py
@@ -0,0 +1,74 @@
+import sys
+
+def is_safe(board, row, col, N):
+    
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    i = row
+    j = col
+    while i >= 0 and j < N:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+
+def solve_nqueens(N, board, row, solutions):
+    
+    if row == N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            solve_nqueens(N, board, row + 1, solutions)
+            board[row][col] = 0
+
+
+def nqueens(N):
+    
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [[0] * N for _ in range(N)]
+    solutions = []
+    
+    solve_nqueens(N, board, 0, solutions)
+    
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    
+    try:
+        N = int(sys.argv[1])
+        nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
