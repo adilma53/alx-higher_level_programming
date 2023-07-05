f5500c76de44a3575cc1af584f58952f4edf7696
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 20:25:46 +0100

    ALX HIGH LEVEL

--- 0x09-python-everything_is_object/101-nqueens.py
+++ 0x09-python-everything_is_object/101-nqueens.py
@@ -1,7 +1,7 @@
 import sys
 
+
 def is_safe(board, row, col, N):
-    
     for i in range(row):
         if board[i][col] == 1:
             return False
@@ -26,7 +26,6 @@ def is_safe(board, row, col, N):
 
 
 def solve_nqueens(N, board, row, solutions):
-    
     if row == N:
         solution = []
         for i in range(N):
@@ -44,19 +43,19 @@ def solve_nqueens(N, board, row, solutions):
 
 
 def nqueens(N):
-    
+
     if not isinstance(N, int):
         print("N must be a number")
         sys.exit(1)
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     board = [[0] * N for _ in range(N)]
     solutions = []
-    
+
     solve_nqueens(N, board, 0, solutions)
-    
+
     for solution in solutions:
         print(solution)
 
@@ -65,7 +64,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
         nqueens(N)
