8cc0d9d249dba27eb4748bfbbc0d6f6ff1c16595
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 16:00:14 +0100

    ALX HIGH LEVEL

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,54 +1,53 @@
 #!/usr/bin/python3
 """
-Matrix Multiplication
+ Matrix Multiplication
+
 """
 
 
-def matrix_multiply(matrix_a, matrix_b):
-    """Multiplies two matrices
+def matrix_mul(m_a, m_b):
     """
-    error_msg1 = " must be a list"
-    error_msg2 = " must be a list of lists"
-    error_msg3 = " should contain only integers or floats"
-    error_msg4a = "each row of "
-    error_msg4b = " must be of the same size"
-    error_msg5 = " can't be empty"
-    error_msg6 = "matrix_a and matrix_b can't be multiplied"
-
-    if type(matrix_a) is not list:
-        raise TypeError("matrix_a" + error_msg1)
-    if type(matrix_b) is not list:
-        raise TypeError("matrix_b" + error_msg1)
-    if not any(isinstance(row, list) for row in matrix_a):
-        raise TypeError("matrix_a" + error_msg2)
-    if not any(isinstance(row, list) for row in matrix_b):
-        raise TypeError("matrix_b" + error_msg2)
-    for row in matrix_a:
-        if len(row) == 0:
-            raise ValueError("matrix_a" + error_msg5)
-    for row in matrix_b:
-        if len(row) == 0:
-            raise ValueError("matrix_b" + error_msg5)
-    for row in matrix_a:
-        for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError("matrix_a" + error_msg3)
-    for row in matrix_b:
-        for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError("matrix_b" + error_msg3)
-    if len(set(len(row) for row in matrix_a)) != 1:
-        raise TypeError(error_msg4a + "matrix_a" + error_msg4b)
-    if len(set(len(row) for row in matrix_b)) != 1:
-        raise TypeError(error_msg4a + "matrix_b" + error_msg4b)
-    if len(matrix_a[0]) != len(matrix_b):
-        raise ValueError(error_msg6)
-
-    new_matrix = []
-    for row in matrix_a:
-        new_row = []
-        for c in range(len(matrix_b[0])):
-            new_row.append(
-                sum(a * b for a, b in zip(row, list(r[c] for r in matrix_b))))
-        new_matrix.append(new_row)
-    return new_matrix
+    Multiples two matrices
+    """
+    test1 = " must be a list"
+    test2 = " must be a list of lists"
+    test3 = " should contain only integers or floats"
+    test4 = "each row of "
+    test4A = " must should be of the same size"
+    V = " can't be empty"
+    V2 = "m_a and m_b can't be multiplied"
+    if type(m_a) is not list:
+        raise TypeError("m_a" + test1)
+    if type(m_b) is not list:
+        raise TypeError("m_b" + test1)
+    if not any(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a" + test2)
+    if not any(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b" + test2)
+    for l in m_a:
+        if len(l) == 0:
+            raise ValueError("m_a" + V)
+    for l in m_b:
+        if len(l) == 0:
+            raise ValueError("m_b" + V)
+    for row in m_a:
+        for e in row:
+            if not isinstance(e, (int, float)):
+                raise TypeError("m_a" + test3)
+    for row in m_b:
+        for e in row:
+            if not isinstance(e, (int, float)):
+                raise TypeError("m_b" + test3)
+    if len(set(len(row) for row in m_a)) != 1:
+        raise TypeError(test4 + "m_a" + test4A)
+    if len(set(len(row) for row in m_b)) != 1:
+        raise TypeError(test4 + "m_b" + test4A)
+    if len(m_a[0]) != len(m_b):
+        raise ValueError(V2)
+    new = []
+    for ar in m_a:
+        nr = []
+        for c in range(len(m_b[0])):
+            nr.append(sum(a * b for a, b in zip(ar, list(r[c] for r in m_b))))
+        new.append(nr)
+    return new
