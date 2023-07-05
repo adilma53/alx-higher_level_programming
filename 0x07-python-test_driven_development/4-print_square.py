6344aabbc3a9cc7b86cdf269f596a04ea362a00a
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 22:25:36 +0100

    ALX HIGH LEVEL

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -18,19 +18,15 @@ def print_square(size):
         TypeError: If size is a float and is less than 0.
 
     """
-    # Check that size is an int
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
 
-    # Check if size is less than zero
     if size < 0:
         raise ValueError('size must be >= 0')
 
-    # Check if size is float and is less than zero
     if isinstance(size, float) and size < 0:
         raise TypeError('size must be an integer')
 
-    # print a square with '#' by size
     for rows in range(size):
         for cols in range(size):
             print('#', end='')
