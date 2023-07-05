6c9f401734d93f10392e9ff28f1a2249e8eb1d47
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 21:32:29 +0100

    ALX HIGH LEVEL

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-class LockedClass(object):
-    __slots__ = 'first_name'
+class LockedClass:
+    __slots__ = "first_name"
+
+    def __init__(self, first_name=None):
+        self. first_name = first_name
