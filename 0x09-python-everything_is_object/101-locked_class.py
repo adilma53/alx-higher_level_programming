7ff2bc7cc39a9fafa0b35ec280bc14504854b7d7
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 21:27:00 +0100

    ALX HIGH LEVEL

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 class LockedClass:
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
+
+    def __setattr__(self, name, value):
+        if not hasattr(self, "first_name") and name != "first_name":
+            raise AttributeError(
+                "'LockedClass' object has no attribute '{}'".format(name))
+        super().__setattr__(name, value)
