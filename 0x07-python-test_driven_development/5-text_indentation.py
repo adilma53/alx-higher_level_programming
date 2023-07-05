d08dcc4cd804ccb93f2ad23cbf82b2cf4a848e30
adil majhoul <itsadilhadid@gmail.com>
2023-07-05 22:22:19 +0100

    ALX HIGH LEVEL

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,14 @@
+
+#!/usr/bin/python3
+
+
+'''
+  Prints a text with 2 new lines after each occurrence of '.', '?', and ':'.
+'''
+
+
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after each occurrence of '.', '?', and ':'.
 
     Args:
         text (str): The input text.
@@ -9,16 +17,12 @@ def text_indentation(text):
         TypeError: If text is not a string.
 
     """
-
     if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    result = ""
-
-    for char in text:
-        result += char
-
-        if char in ['.', '?', ':']:
-            result += "\n\n"
-
-    print(result.strip())
+        raise TypeError('text must be a string')
+    period = text.replace('.', '.\n\n')
+    period = period.replace('?', '?\n\n')
+    period = period.replace(':', ':\n\n')
+    period1 = period.split('\n')
+    for line in range(len(period1)):
+        print("{}".format(period1[line].strip()),
+              end=("" if (line == (len(period1) - 1)) else '\n'))
