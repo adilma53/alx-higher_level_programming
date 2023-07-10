9f831fe29183a1ace4fa25d3ce03d807bf5d6d7a
adil majhoul <itsadilhadid@gmail.com>
2023-07-11 00:24:37 +0100

    ALX HIGH LEVEL

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+bg = BaseGeometry()
+
+
+class Rectangle(bg):
+    """rectangle class"""
+
+    def __init__(self, width, height):
+        """ constructor
+        Args:
+            width (int): width
+            height (int): height
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
