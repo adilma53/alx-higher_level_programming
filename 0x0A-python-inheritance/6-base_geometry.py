39b9d21ceda5a792f2b034ca49021128f0f5825d
adil majhoul <itsadilhadid@gmail.com>
2023-07-11 00:10:53 +0100

    ALX HIGH LEVEL

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class BaseGeometry:
+    """class"""
+
+    def area(self):
+        """ function that raise ("area() is not implemented")"""
+        raise Exception("area() is not implemented")
