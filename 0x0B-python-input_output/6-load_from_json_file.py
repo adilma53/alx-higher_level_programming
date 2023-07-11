d81dc44ebc14e5e2edaf3d15e7d7f7a7db0fb67f
adil majhoul <itsadilhadid@gmail.com>
2023-07-11 21:01:51 +0100

    ALX HIGH LEVEL

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
     """ create string from file that is of json type"""
-    with open(filename, 'a+', encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
