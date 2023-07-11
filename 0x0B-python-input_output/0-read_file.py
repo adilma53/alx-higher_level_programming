7066a80be6242bae2736bb43c7ba1c9c447ded91
adil majhoul <itsadilhadid@gmail.com>
2023-07-11 17:49:04 +0100

    ALX HIGH LEVEL

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """read from file and print to stdout"""
 
 
@@ -9,5 +8,5 @@ def read_file(filename=""):
     Args:
         filename (str, optional): file to read from.
     """
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read(), end='')
