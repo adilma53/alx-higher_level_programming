3a9583bb0bdc31a1352a427fbb783164890b5027
adil_ma <itsadilhadid@gmail.com>
2023-07-17 17:59:16 +0100

    ALX higher level

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
+"""base class"""
+
 class Base:
-    """Base class to manage the id attribute for all future classes."""
+    """base class to manage the id attribute for all future classes"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        Constructor for __nb_objects attribute.
+        constrcutopr for __nb_objects attribute
         """
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
+            self.__nb_objects += 1
             self.id = Base.__nb_objects
