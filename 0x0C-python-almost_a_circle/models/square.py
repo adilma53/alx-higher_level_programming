cc308f8fe2e589bfb9bae503b4d73b0cbc67a7fe
adil_ma <itsadilhadid@gmail.com>
2023-07-18 01:10:13 +0100

    ALX higher level

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+Module defining the Square class.
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class, a subclass of Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square instance.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(
+            self.id, self.x, self.y, self.width
+        )
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square (width and height).
+        """
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Updates the attributes of the square.
+        """
+        argc = len(args)
+        kwargc = len(kwargs)
+        modif_attrs = ['id', 'size', 'x', 'y']
+
+        if argc > 4:
+            argc = 4
+
+        if argc > 0:
+            for i in range(argc):
+                setattr(self, modif_attrs[i], args[i])
+        elif kwargc > 0:
+            for k, v in kwargs.items():
+                if k in modif_attrs:
+                    setattr(self, k, v)
+
+    def to_dictionary(self):
+        """
+        Converts the square object to a dictionary.
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
