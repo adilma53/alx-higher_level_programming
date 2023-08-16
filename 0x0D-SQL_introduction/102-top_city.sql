972dee0d21b4ec1b53b59f1226e9f2344e8dd0fd
adil_ma <itsadilhadid@gmail.com>
2023-08-16 01:52:05 +0100

    sql introduction finished

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 
 -- list top cities by temperature in July and August --
-SELECT city, AVG(temp) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE MONTH(date) BETWEEN 7 AND 8
 GROUP BY city
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
--- order states by names with each state max temperature --
-SELECT state, MAX(temp) AS max_temp
+-- order states by names with each state max temperature
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
-ORDER BY state;
+ORDER BY state
