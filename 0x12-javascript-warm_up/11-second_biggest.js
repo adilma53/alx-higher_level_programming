a4f52e8cd8b05801c253948cba17ace4fb6fd2c7
adil_ma <itsadilhadid@gmail.com>
2023-09-11 23:44:42 +0100

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
-
-
-
 const args = process.argv.slice(2).map(Number);
 
-if (args.length <= 1)
-{
-    console.log(0);
-} else
-{
-    const largest = Math.max(...args);
-    const removeLargest = [...args.filter(num => num !== largest];
-    const secondLargest = Math.max(...removeLargest);
-    console.log(secondLargest);
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  const largest = Math.max(...args);
+  const removeLargest = [...args.filter(num => num !== largest)];
+  console.log(Math.max(removeLargest));
 }
