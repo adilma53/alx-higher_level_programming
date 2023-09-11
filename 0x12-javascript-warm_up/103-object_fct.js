1a0c647aa0f01128265b3fcd22d0a3f77581efb8
adil_ma <itsadilhadid@gmail.com>
2023-09-12 00:12:23 +0100

    task

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const myObject = {
+    type: 'object',
+    value: 12
+};
+
+this.incr = function ()
+{
+    this.value++;
+};
+
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
