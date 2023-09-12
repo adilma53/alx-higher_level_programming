80dfe0ec4f766aa86b0228d203670d2d842cbcc1
adil_ma <itsadilhadid@gmail.com>
2023-09-13 00:08:11 +0100

    finished

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-
 class Rectangle
 {
+
   constructor(w, h)
   {
-    if (w > && h > 0)
+    if (w > 0 && h > 0)
     {
       this.width = w;
       this.height = h;
     }
   }
+
 }
 
 module.exports = Rectangle;
