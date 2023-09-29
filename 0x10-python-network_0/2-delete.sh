2445f4c98a1431f3183b7d32e49c7f9572c2fb6f
adil mae <itsadilhadid@gmail.com>
2023-09-29 03:11:35 +0100

    finished

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# cript that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# sends a DELETE request and displays the body of the response
 curl -sX DELETE "$1"
