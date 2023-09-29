b760a8a71da9909600125a0ba5dbf176bae3a089
adil mae <itsadilhadid@gmail.com>
2023-09-30 00:51:47 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that
+script that
 fetches https://alx-intranet.hbtn.io/status
 """
 
 import requests
 
-if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+request = requests.get("https://alx-intranet.hbtn.io/status")
+print("Body response:")
+print("\t- type: {}".format(type(request.text)))
+print("\t- content: {}".format(request.text))
