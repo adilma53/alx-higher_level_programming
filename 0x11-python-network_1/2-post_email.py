5fd0adf2e40a7c8dbdf9a8ea8d02b30956c8cf89
adil mae <itsadilhadid@gmail.com>
2023-09-30 00:42:20 +0100

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,16 +5,14 @@ the passed URL with the email as a parameter
 """
 import urllib.request
 import urllib.parse
-import sys
+from sys import argv
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    url = argv[1]
+    mail = {"email": argv[2]}
+    data = urllib.parse.urlencode(mail)
+    data = data.encode("ascii")
 
-data = {'email': email}
-
-data = urllib.parse.urlencode(data).encode('utf-8')
-req = urllib.request.Request(url, data)
-
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
-    print("Your email is:", body)
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as reques:
+        print(reques.read().decode("utf-8"))
