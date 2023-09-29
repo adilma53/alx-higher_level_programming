ecc71f74b2a50c8dc6818b7a9cfb1b2fea0be219
adil mae <itsadilhadid@gmail.com>
2023-09-29 03:34:09 +0100

    finished

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,4 +3,13 @@
 
 def find_peak(list_of_integers):
     """find the peak of list of integers"""
-    pass
+    if not list_of_integers:
+        return None
+
+    peak = list_of_integers[0]
+
+    for num in list_of_integers:
+        if num >= peak:
+            peak = num
+
+    return peak
