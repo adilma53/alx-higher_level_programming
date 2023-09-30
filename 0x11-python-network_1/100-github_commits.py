7b856a96752ce5866343dcd8e3f0916d47296855
adil mae <itsadilhadid@gmail.com>
2023-09-30 01:11:24 +0100

    task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-gitHub API
+GitHub API: Fetch the latest 10 commits from a repository
 """
 import requests
 import sys
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    base_url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
+    base_url = f'https://api.github.com/repos/\
+        {owner_name}/{repository_name}/commits'
 
     response = requests.get(base_url)
 
@@ -21,4 +22,5 @@ if __name__ == "__main__":
             author_name = commit['commit']['author']['name']
             print(f'{sha}: {author_name}')
     else:
-        print(f'Error: Unable to fetch commits. Status code: {response.status_code}')
+        print(f'Error: Unable to fetch commits. Status code:\
+                {response.status_code}')
