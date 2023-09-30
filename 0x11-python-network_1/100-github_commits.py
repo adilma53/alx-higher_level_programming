9e86b0f05506f0f436088b67383e4b7c0fcdf0b0
adil mae <itsadilhadid@gmail.com>
2023-09-30 01:17:01 +0100

    task 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,17 +5,17 @@ GitHub API: Fetch the latest 10 commits from a repository
 from sys import argv
 import requests
 
-
-url = "https://api.github.com/repos/{}/{}/commits".format(
-    argv[1], argv[2]
-)
-req = requests.get(url)
-body = req.json()
-try:
-    for i in range(10):
-        print("{}: {}".format(
-            body[i].get("sha"),
-            body[i].get("commit").get("author").get("name")
-        ))
-except IndexError:
-    pass
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[1], argv[2]
+    )
+    req = requests.get(url)
+    body = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                body[i].get("sha"),
+                body[i].get("commit").get("author").get("name")
+            ))
+    except IndexError:
+        pass
