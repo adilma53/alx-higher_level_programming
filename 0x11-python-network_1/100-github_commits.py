b3a01e140b90ee037280ab410fa2a49d522cd5cb
adil mae <itsadilhadid@gmail.com>
2023-09-30 01:06:27 +0100

    task 11

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+gitHub API
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    base_url = f'https://api.github.com/repos/{owner_name}/{repository_name}/commits'
+
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        commits = response.json()
+
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f'{sha}: {author_name}')
+    else:
+        print(f'Error: Unable to fetch commits. Status code: {response.status_code}')
