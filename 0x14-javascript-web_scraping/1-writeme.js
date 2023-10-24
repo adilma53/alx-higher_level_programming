f2880fad00ca8563e2131e35334a15b076e3dd09
adil mae <itsadilhadid@gmail.com>
2023-10-24 23:08:00 +0100

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,8 +5,8 @@ const fs = require('fs');
 const fileToWriteTO = process.argv[2];
 const stringTowrite = process.argv[3];
 
-
 fs.readFile(fileToWriteTO, stringTowrite, (err) => {
   if (err) {
     console.error(err);
+  }
 });
