bdc5d3f58ff70f00230b0d964c078a85823688c7
adil mae <itsadilhadid@gmail.com>
2023-10-24 23:04:44 +0100

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];
+const path = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(path, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
