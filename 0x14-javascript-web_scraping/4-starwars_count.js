c95f3e75cc41a18197b90096a4f147988bf524d2
adil mae <itsadilhadid@gmail.com>
2023-10-25 00:09:06 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+const request = require('request');
+
+const baseUrl = process.argv[2];
+
+function moviesWithCharacter (results, characterId) {
+  let showCount = 0;
+
+  const url = 'https://swapi-api.alx-tools.com/api/people/';
+  results.forEach((show) => {
+    if (show.characters.includes(`${url}${characterId}/`)) {
+      showCount++;
+    }
+  });
+
+  return showCount;
+}
+
+request(baseUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const parsedBody = JSON.parse(body);
+    const results = parsedBody.results;
+
+    const count = moviesWithCharacter(results, 18);
+
+    console.log(count);
+  }
+});
