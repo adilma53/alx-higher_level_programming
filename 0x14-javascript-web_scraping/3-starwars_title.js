d214c764c37addd3c51e63db34b7080ca85a6ce4
adil mae <itsadilhadid@gmail.com>
2023-10-24 23:25:13 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
+
+const baseUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request(baseUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const parsedBody = JSON.parse(body);
+    console.log(parsedBody.title);
+  }
+});
