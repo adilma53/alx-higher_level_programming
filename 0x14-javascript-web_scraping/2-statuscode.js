f830c12c3a60399458cc889da3455eccac5db3ca
adil mae <itsadilhadid@gmail.com>
2023-10-24 23:13:36 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+
+const baseUrl = process.argv[2];
+
+request(baseUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
