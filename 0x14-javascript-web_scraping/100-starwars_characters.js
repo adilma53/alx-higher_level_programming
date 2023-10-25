f0929b527c792317cc05a4a68ca16dd8ff3c6648
adil mae <itsadilhadid@gmail.com>
2023-10-25 02:16:29 +0100

    task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const request = require('request');
+const baseUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request.get(baseUrl, (error, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const parsedBody = JSON.parse(body);
+  const characters = parsedBody.characters;
+  for (const character of characters) {
+    request.get(character, (error, body) => {
+      if (error) {
+        console.log(error);
+      }
+      const parsedBodyCharacters = JSON.parse(body);
+      console.log(parsedBodyCharacters.name);
+    });
+  }
+});
